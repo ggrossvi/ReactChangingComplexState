0a4601582779f9f97bd62ae03623d02924350383
ggrossvi <hello.gloria@gmail.com>
2022-07-15 13:21:09 -0700

    update setFullName to include fName and lName also to use preValue  for previous state.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,58 @@
 import React, { useState } from "react";
 
 function App() {
-  const [fName, setFName] = useState("");
-  const [lName, setLName] = useState("");
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
 
-  function updateFName(event) {
-    const firstName = event.target.value;
-    setFName(firstName);
-  }
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+    // console.log(newValue);
+    // console.log(inputName);
+    //  below resuts in the field not written to be deleted
 
-  function updateLName(event) {
-    const lastName = event.target.value;
-    setFName(lastName);
+    // if (inputName === "fName") {
+    //   setFullName({ fName: newValue });
+    // } else if (inputName === "lName") {
+    //   setFullName({ lName: newValue });
+    //   }
+
+    setFullName((preValue) => {
+      if (inputName === "fname") {
+        return {
+          fName: newValue,
+          lName: preValue.lName
+        };
+      } else if (inputName === "lName") {
+        return {
+          fName: prevValue.fName,
+          lname: newValue
+        };
+      }
+      // console.log(prevValue);
+    });
   }
 
   return (
     <div className="container">
       <h1>
-        Hello {fName} {lName}{" "}
+        Hello {fullName.fName} {fullName.lName}
       </h1>
       <form>
-        <input onChange={updateFName} name="fName" placeholder="First Name" />
-        <input onChange={updateLName} name="lName" placeholder="Last Name" />
+        <input
+          name="fName"
+          onChange={handleChange}
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          onChange={handleChange}
+          placeholder="Last Name"
+          value={fullName.lName} //if I comment out it is uncontrolled component
+        />
         <button>Submit</button>
       </form>
     </div>
